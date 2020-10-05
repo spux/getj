d62ad4750f3e8a295ed1bb356ca8e919ba3de0d0
Melvin Carvalho <melvincarvalho@gmail.com>
2020-10-05 10:41:46 +0200

    add match

--- bin/getj.js
+++ bin/getj.js
@@ -5,8 +5,14 @@ var querySelectorAll = require('query-selector')
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 var cheerio = require('cheerio')
+var argv = require('minimist')(process.argv.slice(2))
 
-var uri = process.argv[2] || 'https://www.bbc.com/news/av/world-europe-54394128'
+globalThis.defaults = {
+  uri: 'https://www.bbc.com/news/av/world-europe-54394128'
+}
+var uri = argv._[0] || defaults.uri
+var match = argv.match
+// console.log(argv)
 
 // console.log('fetching', uri)
 
@@ -23,8 +29,25 @@ fetch(uri)
     scripts.forEach(i => {
       var attributes = i.attributes
       attributes.forEach(j => {
-        if (j.name === 'type' && j.value === 'application/ld+json') {
-          console.log(JSON.stringify(JSON.parse(i.innerHTML), null, 2))
+        if (
+          j.name === 'type' &&
+          (j.value === 'application/ld+json' || j.value === 'application/json')
+        ) {
+          var json = JSON.parse(i.innerHTML)
+          if (match) {
+            var values = Object.values(json)
+            values.forEach(i => {
+              if (Array.isArray(i)) {
+                i.forEach(j => {
+                  if (j?.toString().match(match)) console.log(j)
+                })
+              } else {
+                if (i?.toString().match(match)) console.log(i)
+              }
+            })
+          } else {
+            console.log(JSON.stringify(json, null, 2))
+          }
         }
       })
     })
