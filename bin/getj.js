03f9fd544a83b2ebbf7e9088ad0595ebac10de9c
Melvin Carvalho <https://melvincarvalho.com/#me>
2022-07-20 16:13:50 +0200

    parse files

--- bin/getj.js
+++ bin/getj.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
+var fs = require('fs')
 var fetch = require('node-fetch')
 var DOMParser = require('dom-parser')
-var querySelectorAll = require('query-selector')
-const jsdom = require('jsdom')
-const { JSDOM } = jsdom
-var cheerio = require('cheerio')
 var argv = require('minimist')(process.argv.slice(2))
 
 globalThis.defaults = {
@@ -14,48 +11,111 @@ var uri = argv._[0] || defaults.uri
 var match = argv.match
 // console.log(argv)
 
-// console.log('fetching', uri)
+var protocol = 'http'
+if (!uri?.startsWith('http')) {
+  protocol = 'file'
+}
 
-fetch(uri)
-  .then(function (response) {
-    // The API call was successful!
-    return response.text()
-  })
-  .then(function (html) {
-    var parser = new DOMParser()
-    var dom = parser.parseFromString(html)
 
-    var scripts = dom.getElementsByTagName('script')
-    scripts.forEach(i => {
-      var attributes = i.attributes
-      attributes.forEach(j => {
-        if (
-          j.name === 'type' &&
-          (j.value === 'application/ld+json' || j.value === 'application/json')
-        ) {
-          var json = JSON.parse(i.innerHTML)
-          if (match) {
-            var values = Object.values(json)
-            values.forEach(i => {
-              if (Array.isArray(i)) {
-                i.forEach(j => {
-                  var values = Object.values(j)
-                  values.forEach(k => {
-                    if (k.toString().match(match)) console.log(k)
+if (protocol === 'http') {
+  // console.log('fetching', uri)
+  fetch(uri)
+    .then(function (response) {
+      // The API call was successful!
+      return response.text()
+    })
+    .then(function (html) {
+      var parser = new DOMParser()
+      var dom = parser.parseFromString(html)
+
+      var scripts = dom.getElementsByTagName('script')
+      scripts.forEach(i => {
+        var attributes = i.attributes
+        attributes.forEach(j => {
+          if (
+            j.name === 'type' &&
+            (j.value === 'application/ld+json' || j.value === 'application/json')
+          ) {
+            var json = JSON.parse(i.innerHTML)
+            if (match) {
+              var values = Object.values(json)
+              values.forEach(i => {
+                if (Array.isArray(i)) {
+                  i.forEach(j => {
+                    var values = Object.values(j)
+                    values.forEach(k => {
+                      if (k.toString().match(match)) console.log(k)
+                    })
                   })
-                })
-              } else {
-                if (i?.toString().match(match)) console.log(i)
-              }
-            })
-          } else {
-            console.log(JSON.stringify(json, null, 2))
+                } else {
+                  if (i?.toString().match(match)) console.log(i)
+                }
+              })
+            } else {
+              console.log(JSON.stringify(json, null, 2))
+            }
           }
-        }
+        })
       })
     })
+    .catch(function (err) {
+      // There was an error
+      console.error(err)
+    })
+
+} else if (protocol === 'file') {
+  uri = uri?.replace(/file:[\/]?[\/]?/, '')
+
+  html = fs.readFileSync(uri).toString()
+
+  var parser = new DOMParser()
+  var dom = parser.parseFromString(html)
+
+  var scripts = dom.getElementsByTagName('script')
+  scripts.forEach(i => {
+    var attributes = i.attributes
+    attributes.forEach(j => {
+      if (
+        j.name === 'type' &&
+        (j.value === 'application/ld+json' || j.value === 'application/json')
+      ) {
+        var json = JSON.parse(i.innerHTML)
+        if (match) {
+          var values = Object.values(json)
+          values.forEach(i => {
+            if (Array.isArray(i)) {
+              i.forEach(j => {
+                var values = Object.values(j)
+                values.forEach(k => {
+                  if (k.toString().match(match)) console.log(k)
+                })
+              })
+            } else {
+              if (i?.toString().match(match)) console.log(i)
+            }
+          })
+        } else {
+          console.log(JSON.stringify(json, null, 2))
+        }
+      }
+    })
   })
-  .catch(function (err) {
-    // There was an error
-    console.error(err)
-  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
